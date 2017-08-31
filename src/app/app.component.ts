e24095a053ae1a1330d1e8281cf44a3072cecdb0
mookylicious <adrienguyot17@gmail.com>
2017-08-31 16:19:26 +0200

    operator from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MyCustomObserver } from './mycustomobserver';
+// import { MyCustomObserver } from './mycustomobserver';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
 
@@ -11,20 +11,24 @@ export class AppComponent {
   name = 'Angular';
 
   createObservable() {
-    let numbers = [3, 4, 100, 5];
-    let numbers$ = Observable.create((observer: any) => {
-      for (let n of numbers) {
-        if (n>10){
-          observer.error('trop grand');
-        }
-        observer.next(n);
-      }
-      observer.complete();
-    });
-    numbers$.subscribe(
+    let numbers = [3, 4, 5];
+    // let numbers$ = Observable.create((observer: any) => {
+    //   for (let n of numbers) {
+    //     observer.next(n);
+    //   }
+    //   observer.complete();
+    // });
+    // numbers$.subscribe(
+    //   (val: any) => console.log(val),
+    //   (err: any) => console.log(err),
+    //   () => console.log('Complete !')
+    // )
+    let numbers$ = Observable.from(numbers);
+
+    let sub = numbers$.subscribe(
       (val: any) => console.log(val),
       (err: any) => console.log(err),
-      () => console.log('Complete !')
-    )
+      () => console.log('complete')
+    );
   }
 }
